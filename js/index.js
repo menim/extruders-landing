2f4688cfff2f45c8b31eb481c1cd0116b23ad11a
tol <tol@Mac-mini-tol.local>
2018-04-30 18:02:17 +0300

    add function that that immitate click event download or open in new window pdf file

--- js/index.js
+++ js/index.js
@@ -1,28 +1,41 @@
 'use strict';
 
-     // polyfill for ie
+// polyfill for ie
 
-if (window.NodeList && !NodeList.prototype.forEach) {
-  NodeList.prototype.forEach = function(callback, thisArg) {
-    thisArg = thisArg || window;
-    for(var i = 0; i < this.length; i++) {
-      callback.call(thisArg, this[i], i, this);
-    }
-  };
-}
+(function () {
+  if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function (callback, thisArg) {
+      thisArg = thisArg || window;
+      for (var i = 0; i < this.length; i++) {
+        callback.call(thisArg, this[i], i, this);
+      }
+    };
+  }
 
-var openBtnList = document.querySelectorAll('.is-open');
-var modal = document.querySelector('.modal');
-var overlay = document.querySelector('.overlay');
+  var openBtnList = document.querySelectorAll('.is-open');
+  var modal = document.querySelector('.modal');
+  var overlay = document.querySelector('.overlay');
 
-openBtnList.forEach(function(item) {
-  item.addEventListener('click', function () {
+  openBtnList.forEach(function (item) {
+    item.addEventListener('click', function () {
+      modal.classList.toggle('hide');
+      overlay.classList.toggle('hide');
+    });
+  });
+
+  overlay.addEventListener('click', function () {
     modal.classList.toggle('hide');
     overlay.classList.toggle('hide');
   });
-});
+})();
 
-overlay.addEventListener('click', function () {
-  modal.classList.toggle('hide');
-  overlay.classList.toggle('hide');
-});
+(function () {
+  var button = document.querySelector('.form button');
+  button.addEventListener('click', function () {
+    var link = document.createElement('a');
+    link.href = "catalog.pdf";
+    link.target = "_blank";
+    link.download = "catalog.pdf";
+    link.click();
+  });
+})();
