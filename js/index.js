b0a4ccee46bf6c468126c5fc38d20e1d5d84b63a
tol <tol@Mac-mini-tol.local>
2018-05-02 15:04:19 +0300

    add get pdf file by click on button  crossbrowsing functionality

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
 // polyfill for ie
 
-(function () {
+(function() {
   if (window.NodeList && !NodeList.prototype.forEach) {
-    NodeList.prototype.forEach = function (callback, thisArg) {
+    NodeList.prototype.forEach = function(callback, thisArg) {
       thisArg = thisArg || window;
       for (var i = 0; i < this.length; i++) {
         callback.call(thisArg, this[i], i, this);
@@ -12,40 +12,49 @@
     };
   }
 
-  var openBtnList = document.querySelectorAll('.is-open');
-  var modal = document.querySelector('.modal');
-  var overlay = document.querySelector('.overlay');
+  var openBtnList = document.querySelectorAll(".is-open");
+  var modal = document.querySelector(".modal");
+  var overlay = document.querySelector(".overlay");
 
-  openBtnList.forEach(function (item) {
-    item.addEventListener('click', function () {
-      modal.classList.toggle('hide');
-      overlay.classList.toggle('hide');
+  openBtnList.forEach(function(item) {
+    item.addEventListener("click", function() {
+      modal.classList.toggle("hide");
+      overlay.classList.toggle("hide");
     });
   });
 
-  overlay.addEventListener('click', function () {
-    modal.classList.toggle('hide');
-    overlay.classList.toggle('hide');
+  overlay.addEventListener("click", function() {
+    modal.classList.toggle("hide");
+    overlay.classList.toggle("hide");
   });
 })();
 
-(function () {
-  var button = document.querySelector('.form button');
-  button.addEventListener('click', function () {
-    var link = document.createElement('a');
-    link.href = "catalog.pdf";
-    link.target = "_blank";
+(function() {
+  var button = document.querySelector(".form button");
+  var link = document.createElement("a");
+  link.setAttribute("type", "hidden");
+  link.href = "catalog.pdf";
+  if (typeof link.download != "undefined") {
     link.download = "catalog.pdf";
-    link.click();
+  } else {
+    link.target = "_blank";
+  }
+  button.addEventListener("click", function() {
+    if (link.className === "pdf-link") {
+      link.click();
+    } else {
+      document.body.appendChild(link);
+      link.click();
+    }
   });
 })();
 
-
-(function(){
+(function() {
   var lb = new Lightbox({
     selector: '[data-rel="aiLightbox"]', // string
     lazyload: true, // boolean
     arrows: true, // boolean
     counter: true, // boolean
-    slideSpeed: 500 });  
+    slideSpeed: 500
+  });
 })();
